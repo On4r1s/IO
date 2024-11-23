7b98fddd38f09d3d5d0fa8551ae9878f040099c2
DarkNikito <nikitos.barbados@gmail.com>
2024-11-23 19:08:46 +0100

    added main page

---
+++ extension/js/main.js
@@ -0,0 +1,11 @@
+document.getElementById("calendar-btn").addEventListener("click", () => {
+    alert("Calendar interactivity coming soon!");
+});
+
+document.getElementById("load-recordings-btn").addEventListener("click", () => {
+    alert("Loading recordings...");
+});
+
+document.getElementById("settings-btn").addEventListener("click", () => {
+    alert("Opening settings...");
+});
